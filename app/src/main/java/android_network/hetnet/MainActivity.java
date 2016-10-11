f737e687f264bd25c76b4b4bd6d962153f66fe2a
EDLuke <lukezhang213@gmail.com>
2016-10-10 22:30:13 -0400

    Added the two activities Network manager and System manager as two empty activities.

--- app/src/main/java/android_network/hetnet/MainActivity.java
+++ app/src/main/java/android_network/hetnet/MainActivity.java
@@ -1,7 +1,9 @@
 package android_network.hetnet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Creates System Manager Activity */
+    public void startSystemManager(View view){
+        Intent intent = new Intent(this, SystemManager_Main.class);
+        startActivity(intent);
+    }
+
+    /** Creates Network Manager Activity */
+    public void startNetworkManager(View view){
+        Intent intent = new Intent(this, NetworkManager_Main.class);
+        startActivity(intent);
+    }
+
 }
