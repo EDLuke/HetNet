fe67e38bb2717af07659f9759968e606fda19d9e
EDLuke <lukezhang213@gmail.com>
2016-11-27 16:44:27 -0500

    Fix System Event tracker to use two methods (below or above LOLLIPOP) to find the foreground application.

--- app/src/main/java/android_network/hetnet/system/SystemEventTracker.java
+++ app/src/main/java/android_network/hetnet/system/SystemEventTracker.java
@@ -2,13 +2,19 @@ package android_network.hetnet.system;
 
 import android.app.ActivityManager;
 import android.app.Service;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
 
 import org.greenrobot.eventbus.EventBus;
 
 import java.util.Calendar;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -20,6 +26,8 @@ import static android_network.hetnet.common.Constants.SYSTEM_EVENT_TRACKER;
  */
 
 public class SystemEventTracker extends Service {
+    private static final String LOG_TAG = "SystemEventTracker";
+
     private ScheduledThreadPoolExecutor m_scheduledThreadPoolExecutor;
     private ActivityManager             m_activityManager;
     private Handler                     m_handler;
@@ -45,11 +53,38 @@ public class SystemEventTracker extends Service {
     private class ForegroundPoll implements Runnable{
         @Override
         public void run(){
-            ActivityManager.RunningTaskInfo foregroundTaskInfo = m_activityManager.getRunningTasks(1).get(0);
-            final String newName = foregroundTaskInfo.topActivity.getClassName();
+          String newName = "";
 
-            if(!(newName.equals(m_currentName)))
-                EventBus.getDefault().post(new SystemTriggerEvent(SYSTEM_EVENT_TRACKER, "Foreground Activity Changed", Calendar.getInstance().getTime()));
+          //Source:
+          //http://stackoverflow.com/questions/33581311/android-m-how-can-i-get-the-current-foreground-activity-package-namefrom-a-ser
+          if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            UsageStatsManager usm = (UsageStatsManager) getSystemService(USAGE_STATS_SERVICE);
+            long time = System.currentTimeMillis();
+            List<UsageStats> appList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 1000, time);
+
+            if (appList != null && appList.size() > 0) {
+              SortedMap<Long, UsageStats> mySortedMap = new TreeMap<Long, UsageStats>();
+              for (UsageStats usageStats : appList) {
+                mySortedMap.put(usageStats.getLastTimeUsed(),
+                        usageStats);
+              }
+              if (mySortedMap != null && !mySortedMap.isEmpty()) {
+                newName = mySortedMap.get(mySortedMap.lastKey()).getPackageName();
+              }
+            }
+          }
+          else {
+            ActivityManager am = (ActivityManager) getBaseContext().getSystemService(ACTIVITY_SERVICE);
+            newName = am.getRunningTasks(1).get(0).topActivity.getPackageName();
+          }
+
+          //Fire the event and show in log
+          if(!(newName.equals(m_currentName)) && !(newName.equals(""))) {
+              EventBus.getDefault().post(new SystemTriggerEvent(SYSTEM_EVENT_TRACKER, "Foreground Activity Changed", Calendar.getInstance().getTime()));
+              m_currentName = new String(newName);
+
+              Log.v(LOG_TAG, "Foreground application has been switched to " + newName);
+          }
         }
     }
 }
