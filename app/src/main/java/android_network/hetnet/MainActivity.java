dd80982c8aa09b1ced1be261c6b4b9e5af2e1607
EDLuke <lukezhang213@gmail.com>
2016-11-27 22:58:31 -0500

    Add alternate foreground detection for phones that do not support "Apps with Usage Access"

--- app/src/main/java/android_network/hetnet/MainActivity.java
+++ app/src/main/java/android_network/hetnet/MainActivity.java
@@ -21,7 +21,7 @@ import org.greenrobot.eventbus.ThreadMode;
 import android_network.hetnet.common.trigger_events.UITriggerEvent;
 import android_network.hetnet.policy_engine.PolicyEngine;
 
-//import static android_network.hetnet.common.Constants.LOCATION_EVENT_TRACKER;
+import static android_network.hetnet.common.Constants.LOCATION_EVENT_TRACKER;
 import static android_network.hetnet.common.Constants.NETWORK_EVENT_TRACKER;
 import static android_network.hetnet.common.Constants.POLICY_ENGINE;
 import static android_network.hetnet.common.Constants.SYSTEM_EVENT_TRACKER;
@@ -83,7 +83,7 @@ public class MainActivity extends Activity {
 
   @Subscribe(threadMode = ThreadMode.MAIN)
   public void onMessageEvent(UITriggerEvent event) {
-    if (event.getEventOriginator().equals(NETWORK_EVENT_TRACKER) /*|| event.getEventOriginator().equals(LOCATION_EVENT_TRACKER)*/) {
+    if (event.getEventOriginator().equals(NETWORK_EVENT_TRACKER) || event.getEventOriginator().equals(LOCATION_EVENT_TRACKER)) {
       eventList.setText(event.getEventName() + " event received from " + event.getEventOriginator() + " at " + event.getTimeOfEvent());
       networkList.setText("");
     } else if (event.getEventOriginator().equals(POLICY_ENGINE)) {
--- app/src/main/java/android_network/hetnet/system/SystemEventTracker.java
+++ app/src/main/java/android_network/hetnet/system/SystemEventTracker.java
@@ -1,10 +1,12 @@
 package android_network.hetnet.system;
 
+import android.annotation.TargetApi;
 import android.app.ActivityManager;
 import android.app.Service;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
@@ -51,40 +53,75 @@ public class SystemEventTracker extends Service {
     }
 
     private class ForegroundPoll implements Runnable{
-        @Override
-        public void run(){
-          String newName = "";
+      private final Integer PROCESS_STATE_TOP = 2;
 
-          //Source:
-          //http://stackoverflow.com/questions/33581311/android-m-how-can-i-get-the-current-foreground-activity-package-namefrom-a-ser
-          if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            UsageStatsManager usm = (UsageStatsManager) getSystemService(USAGE_STATS_SERVICE);
-            long time = System.currentTimeMillis();
-            List<UsageStats> appList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 1000, time);
+      @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+      private String getForegroundLollipop(){
+        String ret = "";
+        UsageStatsManager usm = (UsageStatsManager) getSystemService(USAGE_STATS_SERVICE);
+        long time = System.currentTimeMillis();
+        List<UsageStats> appList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 1000, time);
 
-            if (appList != null && appList.size() > 0) {
-              SortedMap<Long, UsageStats> mySortedMap = new TreeMap<Long, UsageStats>();
-              for (UsageStats usageStats : appList) {
-                mySortedMap.put(usageStats.getLastTimeUsed(),
-                        usageStats);
-              }
-              if (mySortedMap != null && !mySortedMap.isEmpty()) {
-                newName = mySortedMap.get(mySortedMap.lastKey()).getPackageName();
-              }
-            }
+        if (appList != null && appList.size() > 0) {
+          SortedMap<Long, UsageStats> mySortedMap = new TreeMap<Long, UsageStats>();
+          for (UsageStats usageStats : appList) {
+            mySortedMap.put(usageStats.getLastTimeUsed(),
+                    usageStats);
           }
-          else {
-            ActivityManager am = (ActivityManager) getBaseContext().getSystemService(ACTIVITY_SERVICE);
-            newName = am.getRunningTasks(1).get(0).topActivity.getPackageName();
-          }
-
-          //Fire the event and show in log
-          if(!(newName.equals(m_currentName)) && !(newName.equals(""))) {
-              EventBus.getDefault().post(new SystemTriggerEvent(SYSTEM_EVENT_TRACKER, "Foreground Activity Changed", Calendar.getInstance().getTime()));
-              m_currentName = new String(newName);
-
-              Log.v(LOG_TAG, "Foreground application has been switched to " + newName);
+          if (mySortedMap != null && !mySortedMap.isEmpty()) {
+            ret = mySortedMap.get(mySortedMap.lastKey()).getPackageName();
           }
         }
+
+        return ret;
+      }
+
+      private String getForeground() {
+        String ret = "";
+        ActivityManager am = (ActivityManager) getBaseContext().getSystemService(ACTIVITY_SERVICE);
+        for (ActivityManager.RunningAppProcessInfo processInfo : am.getRunningAppProcesses()) {
+          if (processInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND &&
+                  processInfo.importanceReasonCode == ActivityManager.RunningAppProcessInfo.REASON_UNKNOWN) {
+            Integer state = null;
+            try{
+              state = ActivityManager.RunningAppProcessInfo.class.getDeclaredField("processState").getInt(processInfo);
+            } catch (Exception e) {
+
+            }
+            if(state != null && state == PROCESS_STATE_TOP) {
+              ret = processInfo.processName;
+            }
+          }
+        }
+
+        return ret;
+      }
+
+      @Override
+      public void run(){
+        String newName = "";
+
+        //Source:
+        //http://stackoverflow.com/questions/33581311/android-m-how-can-i-get-the-current-foreground-activity-package-namefrom-a-ser
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+          newName = getForegroundLollipop();
+
+          //Special cases for certain phones
+          //Source:
+          //http://stackoverflow.com/questions/32135903/devices-without-apps-using-usage-data-or-android-settings-usage-access-setting
+          if(newName.equals(""))
+            newName = getForeground();
+        }
+        else
+          newName = getForeground();
+
+        //Fire the event and show in log
+        if(!(newName.equals(m_currentName)) && !(newName.equals(""))) {
+            EventBus.getDefault().post(new SystemTriggerEvent(SYSTEM_EVENT_TRACKER, "Foreground Activity Changed", Calendar.getInstance().getTime()));
+            m_currentName = new String(newName);
+
+            Log.v(LOG_TAG, "Foreground application has been switched to " + newName);
+        }
+      }
     }
 }
